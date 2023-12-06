57ee951d075010ccbf328c0a495ba897abf0b9eb
asdadsdasd <mkozarez12345@mail.ru>
2023-12-06 10:40:24 +0300

    init commit

---
+++ src/test/java/ru/kozarez/automated_workstation/AutomatedWorkstationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kozarez.automated_workstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedWorkstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
